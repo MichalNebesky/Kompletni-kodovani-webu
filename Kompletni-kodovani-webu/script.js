dc7d6e7cb1773c501b7912ecd5b8da40cc678bb1
Michal Nebeský <nebesky@spsejecna.cz>
2021-11-22 18:50:36 +0100

    Část 2

---
+++ Kompletni-kodovani-webu/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    // Owl Carousel
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+      items: 1,
+      loop: true,
+      dots:false,
+      autoplay:true,
+      autoPlayTimeout: 5000,
+    });
+  });
+  
